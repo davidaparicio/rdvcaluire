40b3276dbd58533ca260c2b46a00398d01d19f19
David Aparicio <david.aparicio@free.fr>
2023-03-08 23:49:39 +0100

    ✨ feat: cancelled context and loop music

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/faiface/beep"
@@ -18,7 +20,8 @@ func main() {
 	// http://rdv.ville-caluire.fr/eAppointment/element/jsp/appointment.jsp
 	const url = "https://rdv.ville-caluire.fr/eAppointment/appointment.do" //https://ville-caluire.toodego.com/page-information-etat-civil/
 	const musicFile = "You_Suffer_by_Napalm_Death.mp3"                     // got from https://archive.org/details/YouSufferNapalmDeath
-	const backoff = 30 * time.Second
+	const backoff = 5 * time.Second
+	const want = http.StatusNotFound
 	var attempt int = 1
 
 	//ctx := context.TODO()
@@ -40,16 +43,41 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer streamer.Close()
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	// ../../../../go/pkg/mod/github.com/hajimehoshi/oto@v1.0.1/context.go:69:12: undefined: newDriver
+	// To fix this error, we need to enable CGO_ENABLED=1
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//https://github.com/faiface/beep/wiki/To-buffer,-or-not-to-buffer,-that-is-the-question
+	buffer := beep.NewBuffer(format)
+	buffer.Append(streamer)
+	err = streamer.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Graceful shutdown goroutine
+	go func(context.CancelFunc) {
+		sigquit := make(chan os.Signal, 1)
+		// os.Kill can't be caught https://groups.google.com/g/golang-nuts/c/t2u-RkKbJdU
+		// POSIX spec: signal can be caught except SIGKILL/SIGSTOP signals
+		// Ctrl-c (usually) sends the SIGINT signal, not SIGKILL
+		// syscall.SIGTERM usual signal for termination
+		// and default one for docker containers, which is also used by kubernetes
+		signal.Notify(sigquit, os.Interrupt, os.Kill, syscall.SIGTERM)
+		sig := <-sigquit
+
+		sugar.Infow("Caught the following signal", "signal", sig)
+		cancel()
+	}(cancel)
 
 	ticker := time.NewTicker(backoff)
-	done := make(chan bool)
 	for {
 		select {
-		case <-ticker.C: // select case must be send or receive
-			//fmt.Printf("Check %d\n", attempt)
+		case <-ticker.C:
 			req, err1 := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 			if err != nil {
 				fmt.Println(err1)
@@ -60,31 +88,27 @@ func main() {
 				fmt.Println(err2)
 				return
 			}
-			//fmt.Println(resp.StatusCode)
-			if resp.StatusCode == http.StatusNotFound {
-				logger.Info("Play music")
-				speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-					done <- true
-				})))
-				<-done
-				logger.Info("Music end")
-				// The loop will continue (and the music can re-run), until the cancellation by the user
-			} else {
-				sugar.Infow("failed to fetch URL",
-					// Structured context as loosely typed key-value pairs.
-					"url", url,
+			if resp.StatusCode == want {
+				sugar.Infow("Check",
 					"attempt", attempt,
 					"statuscode", resp.StatusCode,
-					"backoff", 30*time.Second,
+					"backoff", backoff,
+					"url", url,
 				)
-				//sugar.Infof("Failed to fetch URL: %s", url)
+				music := buffer.Streamer(0, buffer.Len())
+				speaker.Play(music)
+				// The check loop will continue (and the music can re-run), until the cancellation by the user
+			} else {
+				sugar.Infow("unmatch status code",
+					"attempt", attempt,
+					"statuscode", resp.StatusCode,
+					"backoff", backoff,
+					"url", url,
+				)
 			}
 			attempt++
 		case <-ctx.Done():
-			// If the request gets cancelled, log it to STDERR
-			//fmt.Fprint(os.Stderr, "request cancelled\n")
-			logger.Error("request cancelled\n")
-			cancel()
+			sugar.Infow("Graceful shutdown...", ctx.Err().Error())
 			return
 		}
 	}
